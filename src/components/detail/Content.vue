5dd762eabb1a61f51ad78b212182069ca3efe434
madriesen <martijn.driesen5@hotmail.com>
2020-05-17 19:21:08 +0200

    add detail content animation

--- src/components/detail/Content.vue
+++ src/components/detail/Content.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <div v-if="this.location === 'bar'"><bar-content /></div>
-    <div v-else-if="this.location === 'webdev'"><webdev-content /></div>
-    <div v-else-if="this.location === 'sound'"><sound-content /></div>
-    <div v-else></div>
+    <transition name="fade">
+      <div v-if="!page_update">
+        <div v-if="this.location === 'bar'"><bar-content /></div>
+        <div v-else-if="this.location === 'webdev'"><webdev-content /></div>
+        <div v-else-if="this.location === 'sound'"><sound-content /></div>
+        <div v-else></div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -11,6 +15,7 @@
 import Bar from '@/components/detail/content/Bar.vue';
 import Sound from '@/components/detail/content/Sound.vue';
 import Webdev from '@/components/detail/content/Webdev.vue';
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'my-content',
@@ -22,5 +27,36 @@ export default {
     'sound-content': Sound,
     'webdev-content': Webdev,
   },
+
+  computed: {
+    ...mapGetters({
+      page_update: 'update_detail_page',
+    }),
+  },
 };
 </script>
+
+<style lang="scss" scope>
+.fade-leave-active {
+  transition: opacity 500ms;
+}
+
+.fade-enter-active {
+  animation: show 2000ms;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+@keyframes show {
+  0% {
+    opacity: 0;
+  }
+  70% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
